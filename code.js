2758868460a143aa8d3cf16dffd842c239220183
shingo2000 <>
2023-12-08 10:11:50 +0900

    Chat GPTさんに書いてもらったコードを追加

--- code.js
+++ code.js
@@ -1,1 +1,42 @@
-throw new Error("This plugin template uses TypeScript. Follow the instructions in `README.md` to generate `code.js`.")
+"use strict";
+// ファイル内の全ページにわたって各コンポーネントの使用回数を集計し、CSV形式で出力する関数
+function outputComponentUsageAsCSV() {
+    // コンポーネントの使用回数、ライブラリ情報、ノードIDを保持するマップ
+    const componentUsage = {};
+    // 全ページをループ処理
+    figma.root.children.forEach(page => {
+        // 現在のページのすべてのノードを取得
+        const allNodes = page.findAll();
+        // インスタンスのみをフィルタリングし、InstanceNode 型にキャスト
+        const instances = allNodes.filter(node => node.type === "INSTANCE");
+        // 各インスタンスについて処理
+        instances.forEach(instance => {
+            const mainComponent = instance.mainComponent;
+            if (mainComponent) {
+                let componentName;
+                // メインコンポーネントの親がコンポーネントセットの場合はその名前を使用
+                if (mainComponent.parent && mainComponent.parent.type === "COMPONENT_SET") {
+                    componentName = mainComponent.parent.name;
+                }
+                else {
+                    // そうでない場合はメインコンポーネントの名前を使用
+                    componentName = mainComponent.name;
+                }
+                // 使用回数をカウント
+                if (componentUsage[componentName]) {
+                    componentUsage[componentName].count += 1;
+                }
+                else {
+                    componentUsage[componentName] = { count: 1, isRemote: mainComponent.remote, nodeId: mainComponent.id };
+                }
+            }
+        });
+    });
+    // 利用回数で並べ替えてCSV形式の文字列を生成
+    const sortedComponents = Object.entries(componentUsage).sort((a, b) => b[1].count - a[1].count);
+    const csvContent = "Component Name,Usage Count,Is Remote,Node ID\n" + sortedComponents.map(entry => `${entry[0]},${entry[1].count},${entry[1].isRemote},${entry[1].nodeId}`).join("\n");
+    // CSV出力
+    console.log(csvContent);
+}
+// 関数を直接実行
+outputComponentUsageAsCSV();
--- code.ts
+++ code.ts
@@ -1,22 +1,47 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+// ファイル内の全ページにわたって各コンポーネントの使用回数を集計し、CSV形式で出力する関数
+function outputComponentUsageAsCSV() {
+  // コンポーネントの使用回数、ライブラリ情報、ノードIDを保持するマップ
+  const componentUsage: { [key: string]: { count: number; isRemote: boolean; nodeId: string } } = {};
 
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
+  // 全ページをループ処理
+  figma.root.children.forEach(page => {
+    // 現在のページのすべてのノードを取得
+    const allNodes = page.findAll();
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+    // インスタンスのみをフィルタリングし、InstanceNode 型にキャスト
+    const instances = allNodes.filter(node => node.type === "INSTANCE") as InstanceNode[];
+
+    // 各インスタンスについて処理
+    instances.forEach(instance => {
+      const mainComponent = instance.mainComponent;
+      if (mainComponent) {
+        let componentName: string;
+
+        // メインコンポーネントの親がコンポーネントセットの場合はその名前を使用
+        if (mainComponent.parent && mainComponent.parent.type === "COMPONENT_SET") {
+          componentName = mainComponent.parent.name;
+        } else {
+          // そうでない場合はメインコンポーネントの名前を使用
+          componentName = mainComponent.name;
+        }
+
+        // 使用回数をカウント
+        if (componentUsage[componentName]) {
+          componentUsage[componentName].count += 1;
+        } else {
+          componentUsage[componentName] = { count: 1, isRemote: mainComponent.remote, nodeId: mainComponent.id };
+        }
+      }
+    });
+  });
+
+  // 利用回数で並べ替えてCSV形式の文字列を生成
+  const sortedComponents = Object.entries(componentUsage).sort((a, b) => b[1].count - a[1].count);
+  const csvContent = "Component Name,Usage Count,Is Remote,Node ID\n" + sortedComponents.map(entry => `${entry[0]},${entry[1].count},${entry[1].isRemote},${entry[1].nodeId}`).join("\n");
+
+  // CSV出力
+  console.log(csvContent);
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
+// 関数を直接実行
+outputComponentUsageAsCSV();
